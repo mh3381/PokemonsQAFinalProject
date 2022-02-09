8b18d21c8dbc34619a385f87b951e61627874473
mh3381 <hritcumichael@gmail.com>
2022-02-09 12:21:57 +0200

    generated @bean in application.java

--- src/main/java/com/qa/pokemons/PokemonsApplication.java
+++ src/main/java/com/qa/pokemons/PokemonsApplication.java
@@ -1,10 +1,23 @@
 package com.qa.pokemons;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.qa.pokemons.domain.Pokemon;
 
 @SpringBootApplication
 public class PokemonsApplication {
+	
+	@Bean
+	public List<Pokemon> makeListBean() {
+		List<Pokemon> pokemon = new ArrayList<>();
+		pokemon.add(new Pokemon(0, "Mayiev", "Orange", 998));
+		return pokemon;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonsApplication.class, args);
